35f8206b8a8a2c407b003a8db79adecb6249e2a3
Use Your Mind <arhiv@LAPTOP-F4MEEIDG.localdomain>
2022-10-11 23:04:30 +0300

    Фикс 5 шага

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -8,7 +8,7 @@ def main():
     def welcome_user():
         name = prompt.string('May I have your name? ')
         print('Hello, ' + name + '!')
-        print('Answer "yes" if the number is even, otherwise answer "no"')
+        print('Answer "yes" if the number is even, otherwise answer "no".')
 
         cor_sum = 0
         i = 1
